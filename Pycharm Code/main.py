460b5208cd8f3fcbbb4bfce40c7da122b9d980dd
Brandon <brandonh291@charter.net>
2025-04-15 22:13:30 -0500

    Update main.py

--- Pycharm Code/main.py
+++ Pycharm Code/main.py
@@ -8,7 +8,7 @@
 
 class Propeller:
     def __init__(self,diameter,pitch,efficiency):
-        self.pitch=pitch
+        self.pitch=pitch # inches
         self.diameter=diameter
         self.efficiency=efficiency
 
@@ -18,13 +18,28 @@ class Propeller:
         p3=pow(RPM*0.0254*self.pitch*(1/60),2)
         p4=(RPM*0.0254*self.pitch*(1/60)*airspeed)
         p5=pow(self.diameter/(3.29546*self.pitch),1.5)
-        return p1*p2*(p3-p4)*p5
+        return p1*p2*(p3-p4)*p5 #newtons
 
+class Air:
+    def __init__(self):
+        self.density=1.225 #kg/m^3
+        self.temp = 59 # C
+        self.altitude= 0 # m above sea level
+    def calc_density(self,alt):
+        self.calc_Temp(alt)
+        pressure=101.29*pow(((self.temp+273.1)/288.08),5.256) # kilopascal
+        self.density=pressure/(0.2869*(self.temp+273.1)) # kg/m^3
+        return self.density
 
+    def calc_Temp(self,alt):
+        self.temp=15.04-0.00649*alt # Celcius
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     prop=Propeller(10,6,0.9)
     print(prop.dynamic_Thrust(10600,0,1.225))
+    env=Air()
+    print(env.calc_density(0))
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
