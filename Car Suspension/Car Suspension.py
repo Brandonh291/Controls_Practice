b70e8b7b6f606357b835376af00cb96ac5689066
Brandon <brandonh291@charter.net>
2025-05-05 20:58:25 -0500

    Create Car Suspension.py

---
+++ Car Suspension/Car Suspension.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May  5 20:48:35 2025
+
+@author: brand
+"""
+import random
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+
+# Modeling a Mass dropped dropped from a variable height, modeled as two mass
+# with a spring and dampener between them
+
+
+
+# The goal will be to model it in two states: free-fall, and with a fixed side,
+# the assumption is that the portion touching the ground will act asa fixed compression point
+
+car_Mass=100
+wheel_mass=1
+spring_const=1
+damp_const= 1
+
